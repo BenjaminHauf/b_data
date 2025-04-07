280c3da403f9aab2e42d39f5922b78ba8d4c1271
BenjaminHauf <benx@ben-hauf.de>
2025-04-07 11:24:38 +0200

    Included Address Model

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,30 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
+class Address(models.Model):
+
+    street = models.CharField(max_length=30, null=True, blank=True)
+    house_number = models.IntegerField(null=True, blank=True)
+    addition = models.CharField(null=True, blank=True)
+    zip_code = models.IntegerField(null=True, blank=True)
+    city = models.CharField(max_length=30, null=True, blank=True)
+    state = models.CharField(max_length=30, null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.street} {self.house_number}{self.addition} {self.zip_code} {self.city}"
+    
+
+class Contact(models.Model):
+
+    first_name = models.CharField(max_length=30, null=True, blank=True)
+    last_name = models.CharField(max_length=30, null=True, blank=True)
+    contact_email = models.CharField(max_length=30, null=True, blank=True)
+    contact_phone = models.IntegerField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.first_name}{self.last_name} {self.contact_email} {self.contact_phone}"
+
+
 class Account(models.Model):
     
     first_name = models.CharField(max_length=30, null=False, blank=False)
@@ -12,7 +36,7 @@ class Account(models.Model):
     birthplace = models.CharField(max_length=30, null=True, blank=True)
     image = models.CharField(null=True,blank=True)
     phone_number = models.BigIntegerField(null=True, blank=True)
-    address = models.CharField(max_length=30, null=True, blank=True)
+    address = models.ForeignKey(Address, null=True, blank=True, on_delete=models.SET_NULL)
 
     def save(self, *args, **kwargs):
         if self.pk is not None:
@@ -32,13 +56,8 @@ class Account(models.Model):
 class Family(models.Model):
 
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
-
-    first_name = models.CharField(max_length=30, null=False, blank=False)
-    last_name = models.CharField(max_length=30, null=False, blank=False)
-    address = models.CharField(max_length=30, null=True, blank=True)
-    
-    phone = models.BigIntegerField(null=True, blank=True)
-    email = models.EmailField(max_length=30, null=True, blank=True)
+    address = models.ForeignKey(Address, null=True, blank=True, on_delete=models.SET_NULL)
+    contact = models.ForeignKey(Contact, null=True, blank=True, on_delete=models.SET_NULL)
     main_contact = models.BooleanField(default=False)
 
     def __str__(self):
@@ -51,39 +70,31 @@ class Family(models.Model):
 class Accommodation(models.Model):
 
     name = models.CharField(max_length=30, null=True, blank=True)
-    address = models.CharField(max_length=30, null=True, blank=True)
+    address = models.ForeignKey(Address, null=True, blank=True, on_delete=models.SET_NULL)
+    contact = models.ForeignKey(Contact, null=True, blank=True, on_delete=models.SET_NULL)
+    cost = models.CharField(max_length=30, null=True, blank=True)
     start_date = models.DateField()
-    contact_name = models.CharField(max_length=30, null=True, blank=True)
-    contact_email = models.CharField(max_length=30, null=True, blank=True)
-    contact_phone = models.CharField(max_length=30, null=True, blank=True)
-
 
 class Bank(models.Model):
-    name = models.CharField(max_length=30, null=True, blank=True)
+    company_name = models.CharField(max_length=30, null=True, blank=True)
+    contact = models.ForeignKey(Contact, null=True, blank=True, on_delete=models.SET_NULL)
+    address = models.ForeignKey(Address, null=True, blank=True, on_delete=models.SET_NULL)
     account_number = models.CharField(max_length=30, null=True, blank=True)
-    address = models.CharField(max_length=30, null=True, blank=True)
     iban = models.CharField(max_length=30, null=True, blank=True)
     start_date = models.DateField()
-    contact = models.CharField(max_length=30, null=True, blank=True)
-    contact_email = models.CharField(max_length=30, null=True, blank=True)
-    contact_phone = models.CharField(max_length=30, null=True, blank=True)
 
 class Insurance(models.Model):
     company_name = models.CharField(max_length=30, null=True, blank=True)
+    address = models.ForeignKey(Address, null=True, blank=True, on_delete=models.SET_NULL)
+    contact = models.ForeignKey(Contact, null=True, blank=True, on_delete=models.SET_NULL)
     product = models.CharField(max_length=30, null=True, blank=True)
     cost = models.CharField(max_length=30, null=True, blank=True)
-    contact = models.CharField(max_length=30, null=True, blank=True)
     start_date = models.DateField()
-    address = models.CharField(max_length=30, null=True, blank=True)
-    contact_email = models.CharField(max_length=30, null=True, blank=True)
-    contact_phone = models.CharField(max_length=30, null=True, blank=True)
 
 class Administration(models.Model):
-    name = models.CharField(max_length=30, null=True, blank=True)
-    address = models.CharField(max_length=30, null=True, blank=True)
+    organisation = models.CharField(max_length=30, null=True, blank=True)
+    address = models.ForeignKey(Address, null=True, blank=True, on_delete=models.SET_NULL)
+    contact = models.ForeignKey(Contact, null=True, blank=True, on_delete=models.SET_NULL)
     iban = models.CharField(max_length=30, null=True, blank=True)
     start_date = models.DateField()
-    contact = models.CharField(max_length=30, null=True, blank=True)
-    contact_email = models.CharField(max_length=30, null=True, blank=True)
-    contact_phone = models.CharField(max_length=30, null=True, blank=True)
     reference_number = models.CharField(max_length=30, null=False, blank=False)
